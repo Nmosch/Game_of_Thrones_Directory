09025473dbdd7c04af0db001eb19fa63815a36cc
NMosch <nmosch@gmail.com>
2020-07-18 14:19:23 -0700

    Updates

---
+++ precache-manifest.b7220342375f7fdaa0acaf161c5e17b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f459b8ea50af30f4839efac9c0136780",
+    "url": "/Game_of_Thrones_Directory/index.html"
+  },
+  {
+    "revision": "2739b6bcd206956fa057",
+    "url": "/Game_of_Thrones_Directory/static/css/main.368510ad.chunk.css"
+  },
+  {
+    "revision": "a0c082365fa05ccb5a73",
+    "url": "/Game_of_Thrones_Directory/static/js/2.b5f76a14.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Game_of_Thrones_Directory/static/js/2.b5f76a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2739b6bcd206956fa057",
+    "url": "/Game_of_Thrones_Directory/static/js/main.4cb5e2b8.chunk.js"
+  },
+  {
+    "revision": "6c68b891ee87e5dffc8f",
+    "url": "/Game_of_Thrones_Directory/static/js/runtime-main.73505cce.js"
+  }
+]);
